9617af5cbaef65c8bd040e6e20c5561e7362c914
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-04 19:28:53 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{useState} from "react"
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   const [toggledrawersidebar,settogledrawersidebar]=useState({
     display:"none"
   });
-  
+  const toggledrawer=()=>{
+    if(toggledrawersidebar.display==="none"){
+      settogledrawersidebar({
+        display:"flex",
+      });
+    }else{
+      settogledrawersidebar({
+        display:"none",
+      });
+    }
+  }
+  const [editcreatechanelbtn ,seteditcreatechanelbtn] =useState(false)
+  const[videouploadpage,setvideouploadpage]=useState(false);
   return (
-    
+    <Navbar seteditcreatechenelbtn={seteditcreatechanelbtn }/>
   );
 }
 
